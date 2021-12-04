48506622cae8530325bb780ed2061a9165fa11cd
Vimal Dubey <vimaldubey123@outlook.com>
2021-12-04 23:21:25 +0530

    Done 95% app...

--- app/src/main/java/com/vdcodeassociate/fitme/constants/Constants.kt
+++ app/src/main/java/com/vdcodeassociate/fitme/constants/Constants.kt
@@ -63,8 +63,7 @@ object Constants {
 
         // Notifications Constants
         const val NOTIFICATION_CHANNEL_ID = "tracking_channel"
-
         const val NOTIFICATION_CHANNEL_NAME = "Tracking"
-
         const val NOTIFICATION_ID = 1
+
 }
