eafa95581968f8bddebcb76836f53e1d27d9efb1
Vimal Dubey <vimaldubey123@outlook.com>
2021-12-06 18:41:27 +0530

    Done 95% app...

--- app/src/main/java/com/vdcodeassociate/fitme/constants/Constants.kt
+++ app/src/main/java/com/vdcodeassociate/fitme/constants/Constants.kt
@@ -40,11 +40,8 @@ object Constants {
 
         // Tracking Service Constants
         const val ACTION_START_OR_RESUME_SERVICE = "ACTION_START_OR_RESUME_SERVICE"
-
         const val ACTION_PAUSE_SERVICE = "ACTION_PAUSE_SERVICE"
-
         const val ACTION_STOP_SERVICE = "ACTION_STOP_SERVICE"
-
         const val ACTION_SHOW_TRACKING_FRAGMENT = "ACTION_SHOW_TRACKING_FRAGMENT"
 
         // Location Update Constant
