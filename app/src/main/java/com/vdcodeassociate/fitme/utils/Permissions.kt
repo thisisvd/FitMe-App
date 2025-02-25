1ace1f5fbfe595ea3b881292d3cac1183d374fd8
Vimal Dubey <vimaldubey123@outlook.com>
2025-02-26 01:43:50 +0530

    updated permissions for android 14 crashes

--- app/src/main/java/com/vdcodeassociate/fitme/utils/Permissions.kt
+++ app/src/main/java/com/vdcodeassociate/fitme/utils/Permissions.kt
@@ -1,25 +1,50 @@
 package com.vdcodeassociate.fitme.utils
 
+import android.Manifest
 import android.content.Context
+import android.os.Build
 import androidx.fragment.app.Fragment
 import com.vdcodeassociate.fitme.constants.Constants.PERMISSION_LOCATION_REQUEST_CODE
 import pub.devrel.easypermissions.EasyPermissions
 
 object Permissions {
 
-    fun hasLocationPermission(context: Context) =
-        EasyPermissions.hasPermissions(
-            context,
-            android.Manifest.permission.ACCESS_FINE_LOCATION,
-        )
+    fun hasLocationPermission(context: Context): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            EasyPermissions.hasPermissions(
+                context,
+                *arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.POST_NOTIFICATIONS
+                )
+            )
+        } else {
+            EasyPermissions.hasPermissions(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        }
+    }
 
     fun requestLocationPermission(fragment: Fragment) {
-        EasyPermissions.requestPermissions(
-            fragment,
-            "This application cannot work without Location Permission",
-            PERMISSION_LOCATION_REQUEST_CODE,
-            android.Manifest.permission.ACCESS_FINE_LOCATION
-        )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            EasyPermissions.requestPermissions(
+                fragment,
+                "This application cannot work without Location Permission",
+                PERMISSION_LOCATION_REQUEST_CODE,
+                *arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.POST_NOTIFICATIONS
+                )
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                fragment,
+                "This application cannot work without Location Permission",
+                PERMISSION_LOCATION_REQUEST_CODE,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        }
     }
 
 }
